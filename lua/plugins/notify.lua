ce1aa4be85fadab7e2a13e3d4dc270b231387f59
tsukiyx <noir.xen@gmail.com>
2024-11-22 02:12:20 -0500

    notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -4,8 +4,21 @@ return {
 	config = function()
 		local notify = require("notify")
 		vim.notify = notify
-		notify('Welcome Back, Noir')
-		require('notify').setup({
+
+		-- Función para mostrar solo el nombre del archivo
+		local function show_save_notification()
+			local file_name = vim.fn.fnamemodify(vim.fn.expand('%'), ":t") -- :t toma solo el nombre del archivo
+			notify('Archivo guardado: ' .. file_name)
+		end
+
+		-- Llamada a la función de notificación personalizada al guardar
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			pattern = "*",
+			callback = show_save_notification
+		})
+
+		-- Configuración del notify
+		notify.setup({
 			background_colour = '#000000',
 		})
 	end
